f10ae6f8b75904c898e8533089977377722bbbed
Alex Kavanagh <144698674+AlexanderKav@users.noreply.github.com>
2025-02-25 10:11:58 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,27 +1,39 @@
 from flask import Flask, render_template, json
 
-import random, requests
+import random, requests, json
 
 app = Flask(__name__)
 
 
 
+
 # Load JSON data from the .txt file
 bike_path = 'bikes_2025-02-24_16-12-33.txt' 
-#weather_path = 'current_weather_2025-02-24_15-17-44.txt'
-weather_data = 'current_weather_2025-02-24_15-17-44.txt'
-
-with open(weather_data, 'r') as file:
-    weather_data = json.load(file)  # Parse the JSON content
-
 
 with open(bike_path, 'r') as file:
     bike_stations = json.load(file)  # Parse the JSON content
 
 bike_station = random.choice(bike_stations)
 
-temp = weather_data["temp"]
-wind_speed = weather_data["wind_speed"]
+
+# OpenWeather API endpoint
+API_KEY = "5361004f3b8168405f9c7f15918a4186"
+LAT = bike_station['position']["lat"]  
+LON = bike_station['position']["lng"]  
+UNITS = "Fahrenheit"  # Use "imperial" for Fahrenheit
+
+url = f"http://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units={UNITS}"
+
+
+response = requests.get(url)
+if response.status_code == 200:
+    weather_data = response.json()
+    print(json.dumps(weather_data, indent=4))  # Pretty print the JSON data
+else:
+    print(f"Error: Unable to fetch weather data. Status code {response.status_code}")
+
+temp = weather_data["main"]["temp"]
+wind_speed = weather_data["wind"]["speed"]
 weather_main = weather_data["weather"][0]["main"]
 @app.route('/')
 def home():
@@ -29,3 +41,5 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
