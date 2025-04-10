4cb90bbe527a9998e1e875e560b3df4e94b7c9ca
Xinchi Jian <81400416+Justetete@users.noreply.github.com>
2025-04-10 23:28:08 +0100

    Update JCD_DB_local.py
    Juster's EC2 mySql server

--- app/database/JCD_DB_local.py
+++ app/database/JCD_DB_local.py
@@ -1,7 +1,7 @@
-#The Amazon AWS RDS information for connecting to the Database
+#The EC2 mySql server information for connecting to the Database
 
 USER = 'root' #user name
-PASSWORD = 'Just1996' #password
+PASSWORD = 'password' #password
 PORT = '3306' #port access number for RDS
-DB = 'local_db_jcdecaux' #database schema
+DB = 'mydb' #database schema
 URI = '127.0.0.1' #URI for RDS database
