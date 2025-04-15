36abce4277b822986e59dc463a47073d161c329c
Mark Tully <167004432+mtully-64@users.noreply.github.com>
2025-04-15 21:37:09 +0100

    Delete app/database/JCD_API_Info.py

--- app/database/JCD_API_Info.py
+++
@@ -1,6 +0,0 @@
-#My API Key for JCDecaux
-JCKEY = "1bee4dcd68c8e76a61647efcc43b3e68a55cacaf"
-
-#The area/city I wish to view, i.e. Dublin
-NAME = "dublin"
-STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
