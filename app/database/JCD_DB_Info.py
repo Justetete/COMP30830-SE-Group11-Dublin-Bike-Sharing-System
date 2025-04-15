88ec26e858573f90d7719ee591d8191e944fe0fb
Mark Tully <167004432+mtully-64@users.noreply.github.com>
2025-04-15 21:37:25 +0100

    Delete app/database/JCD_DB_Info.py

--- app/database/JCD_DB_Info.py
+++
@@ -1,7 +0,0 @@
-# MySql database information
-
-USER = 'mark' #user name
-PASSWORD = 'Johndeere10!' #password
-PORT = '3306' #port access number for RDS
-DB = 'database-download-jc-decaux' #database schema
-URI = 'database-download-jc-decaux.cp42yssukjwp.eu-west-1.rds.amazonaws.com' #URI for RDS database
