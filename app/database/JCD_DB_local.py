ea91af2217b956c711456074443fb2b94733e735
Mark Tully <167004432+mtully-64@users.noreply.github.com>
2025-04-15 21:37:44 +0100

    Delete app/database/JCD_DB_local.py

--- app/database/JCD_DB_local.py
+++
@@ -1,7 +0,0 @@
-#The EC2 mySql server information for connecting to the Database
-
-USER = 'root' #user name
-PASSWORD = 'password' #password
-PORT = '3306' #port access number for RDS
-DB = 'mydb' #database schema
-URI = '127.0.0.1' #URI for RDS database
